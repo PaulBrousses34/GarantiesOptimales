8c4dc5c52a4c09cff163e4215700d4a037f116af
PaulBrousses <paul.brousses@gmail.com>
2020-09-24 11:37:50 +0200

    fixMail5

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -96,7 +96,7 @@ class HomeController extends AbstractController
             ->to('contact@garanties-optimales.com')
             ->subject('Formulaire de contact')
             ->htmlTemplate('email/contact/send.html.twig')
-            ->attachFromPath('/assets/images/attachment/'.$fileName.'')
+            ->attachFromPath('/../assets/images/attachment/'.$fileName.'')
             ->context([
                 'mail' => $email,
                 'subject' => $subject,
