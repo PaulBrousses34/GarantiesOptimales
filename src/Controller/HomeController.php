594685d96fddf4d5a4aa6e58e441aedf6fe843c4
PaulBrousses <paul.brousses@gmail.com>
2020-09-24 11:18:53 +0200

    fixMail1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -88,7 +88,7 @@ class HomeController extends AbstractController
                     $fileName = $fileUploader->saveFile($form['file'], '/../assets/images/attachment');
             
             if (isset($fileName)) {
-                        $file = file_get_contents('assets/images/attachment/'.$fileName.'');
+                        //$file = file_get_contents('assets/images/attachment/'.$fileName.'');
    
                         $emailToSend = (new TemplatedEmail())
 
@@ -96,7 +96,7 @@ class HomeController extends AbstractController
             ->to('contact@garanties-optimales.com')
             ->subject('Formulaire de contact')
             ->htmlTemplate('email/contact/send.html.twig')
-            ->attach($file)
+            ->attach('assets/images/attachment/'.$fileName.'')
             ->context([
                 'mail' => $email,
                 'subject' => $subject,
@@ -104,7 +104,7 @@ class HomeController extends AbstractController
                 'type' => $type,
                 'nom' => $nom,
                 'prenom' => $prenom,
-                'file' => $file
+
             ]);
                 
                         $mailer->send($emailToSend);
